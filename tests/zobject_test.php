6c93cc2c3f0004d4765612c8d4b043b5c5123840
Benjamin <bhoogter@yahoo.com>
2020-09-29 16:24:52 -0400

    Fix test setup, deploy

---
+++ tests/zobject_test.php
@@ -0,0 +1,17 @@
+<?php
+
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class zobject_test extends TestCase
+{
+    public static function setUpBeforeClass(): void
+    {
+    }
+
+    public function testLayout(): void {
+        $this->assertTrue(true);
+    }
+}
